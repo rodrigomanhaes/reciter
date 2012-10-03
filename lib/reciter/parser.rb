5d7c312a356b94d72f741b08859ca73f76d67f98
Rodrigo Manhães <rmanhaes@gmail.com>
2012-10-03 09:58:07 -0300

    parse single numbers

--- lib/reciter/parser.rb
+++ lib/reciter/parser.rb
@@ -1,4 +1,7 @@
 module Reciter
   class Parser
+    def parse(sequence)
+      [sequence.to_i]
+    end
   end
 end
--- spec/reciter_spec.rb
+++ spec/reciter_spec.rb
@@ -1,4 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Reciter::Parser do
+  describe 'to list' do
+    it 'parses single numbers' do
+      subject.parse('1').should == [1]
+      subject.parse('50').should == [50]
+      subject.parse('987654').should == [987654]
+    end
+  end
 end
